c37dfef19ce3fdc4e94e7dc1e9303a4ce1465dae
Vinicius Petini <petiniprojetos96@gmail.com>
2024-12-11 17:13:15 -0400

    Alterations on infra ngrok

--- certs/uhosts.sh
+++ certs/uhosts.sh
@@ -1,4 +1,4 @@
-echo "127.0.0.1 viewer.chatbot.msq" | sudo tee -a /etc/hosts
-echo "127.0.0.1 builder.chatbot.msq" | sudo tee -a /etc/hosts
+echo "127.0.0.1 viewer.typebot.msq" | sudo tee -a /etc/hosts
+echo "127.0.0.1 builder.typebot.msq" | sudo tee -a /etc/hosts
 echo "127.0.0.1 n8n.msq" | sudo tee -a /etc/hosts
 echo "127.0.0.1 evolution.msq" | sudo tee -a /etc/hosts
